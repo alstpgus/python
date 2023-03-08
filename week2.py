73a0e82dde7d54971e1675d87e289406221cf1c5
alstpgus <127291576+alstpgus@users.noreply.github.com>
2023-03-08 18:37:42 +0900

    Add files via upload

---
+++ week2.py
@@ -0,0 +1,34 @@
+# name = 'sehyun'
+# num =1024
+
+# print(name, num)
+# print(name + num)   #서로 다른 자료형끼리 + 연산을 하면 오류가 난다
+
+###포멧팅(formatting)
+# name = 'sehyun'
+# grade = 1
+# clas = 5
+
+# print("1학년 5반 sehyun입니다.")
+# print("%d학년 %d반 %s 입니다." %(grade, clas, name))   #포맷기호
+# print("{}학년 {}반 {} 입니다.".format(grade, clas, name))  #포맷함수
+# print(f"{grade}학년 {clas}반 {name}입니다. ") #f-string
+
+# name = "sehyun"
+# age = 14
+# height = 157.6
+
+# print("이름: sehyun, 나이: 14살, 키: 157.6cm")
+# print("이름: %s, 나이: %d살, 키: %fcm" %(name, age, height))
+# print("이름: {}, 나이: {}살, 키: {}cm".format(name, age, height))
+# print(f"이름: {name}, 나이: {age}살, 키: {height}cm")
+
+###input() 함수 : 입력
+# num = input("숫자를 입력하세요: " )
+# print(f"입력하신 숫자는 {num}입니다.")
+
+# name = input("이름을 입력하세요: ")
+# print(f"{name}님 환영합니다.")
+
+
+
